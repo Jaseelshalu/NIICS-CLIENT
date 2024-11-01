dccc708bf3bd3e08e503bcd6ae664b2f09aba18b
MuhammedHashimT <hashimt4567@gmail.com>
2024-11-01 14:34:52 +0530

    chore: Add axios dependency and implement applicant creation in applicantStore.ts

--- src/store/applicantStore.ts
+++ src/store/applicantStore.ts
@@ -1,5 +1,7 @@
 import { Applicant } from "@/types/types";
+import toast from "react-hot-toast";
 import { create } from "zustand";
+import axios from "axios";
 
 interface ApplicantStoreState {
   applicants: Applicant[];
@@ -20,7 +22,43 @@ const useApplicantStore = create<ApplicantStoreState>((set) => ({
   setApplicants: (applicants) => set({ applicants }),
   applicant: null,
   setApplicant: (applicant) => set({ applicant }),
-  createApplicant: async (applicant) => {},
+  createApplicant: async (applicant) => {
+    const loadingToast = toast.loading("Creating applicant...");
+    try {
+      await axios
+        .post(`${import.meta.env.API_URL}/applicant`, applicant)
+        .then((response) => {
+          console.log(response.data);
+          if (response.status === 201) {
+            toast.success("Applicant created successfully", {
+              id: loadingToast,
+              duration: 3000,
+            });
+          } else if (response.status === 200) {
+            toast.error(response?.data?.error || `Failed to create applicant`, {
+              id: loadingToast,
+              duration: 3000,
+            });
+          } else {
+            toast.error(`Failed to create applicant`, {
+              id: loadingToast,
+              duration: 3000,
+            });
+          }
+        })
+        .catch((error) => {
+          toast.error(`Failed to create applicant`, {
+            id: loadingToast,
+            duration: 3000,
+          });
+        });
+    } catch (error) {
+      toast.error(`Failed to create applicant`, {
+        id: loadingToast,
+        duration: 3000,
+      });
+    }
+  },
   getApplicants: async () => {},
   getApplicant: async (id) => {},
   updateApplicant: async (applicant) => {},
