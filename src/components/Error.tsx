413ad2425247bed0f18285a908ca4824d9ff1df2
MUHAMMED HISHAM AC <cdg.prjs@gmail.com>
2024-11-09 14:49:01 +0530

    fix: Update error handling and correct maxMarks to maxMark in MarkColumns

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -8,7 +8,7 @@ export default function Error() {
   return (
     <>
 
-        // error.status === 404 &&
+      {/* // error.status === 404 && */}
       <div className="flex min-h-screen flex-col items-center justify-center p-4 text-center">
         <div className="mb-8">
           {/* <CloudSunIcon className="text-[#F9A826] text-6xl" /> */}
@@ -21,18 +21,22 @@ export default function Error() {
             {/* <p className="mb-8 text-lg text-gray-500">Sorry, this page have some issue.</p> */}
           </>
         }
-        <div>
-          <h1 className="mb-2 text-6xl font-bold text-gray-800">{error.status}</h1>
-          <p className="mb-4 text-2xl font-light text-gray-600">{error.statusText || error.message}</p>
-          <p className="mb-8 text-lg text-gray-500">Sorry, this page have some issue.</p>
-        </div>
+        {
+          error.status !== 404 &&
+          <div>
+            <h1 className="mb-2 text-6xl font-bold text-gray-800">{error.status}</h1>
+            <p className="mb-4 text-2xl font-light text-gray-600">{error.statusText || error.message}</p>
+            <p className="mb-8 text-lg text-gray-500">Sorry, this page have some issue.</p>
+          </div>
+        }
+
 
         <Link to={'/'} className="mb-4 inline-block text-sm">
           <Button variant="default">Go back home</Button>
         </Link>
-        <div className="mt-16">
+        {/* <div className="mt-16">
           <CatIcon className="text-gray-300 text-9xl" />
-        </div>
+        </div> */}
       </div>
 
 
